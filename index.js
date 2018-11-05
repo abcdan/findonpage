5f2032e961070dce4e0a253e3c51c47fdc397200
Like <dahnjohl@gmail.com>
2018-11-05 21:58:51 +0100

    :tada: initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+function checkOnpage(url,word){
+var request = require("request");
+const fakeUa = require('fake-useragent');
+
+// We need to generate some fake headers for some websites, which require headers before passing on the page.
+var headers = {
+  'User-Agent': fakeUa()
+};
+request.get({ url: url, headers: headers }, function (e, r, body) {
+    var regex = new RegExp(eval("/"+word+"/"))
+    console.log(regex.test(body))
+  });
+   
+}
